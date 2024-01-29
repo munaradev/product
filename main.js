c9ba17c58a4e85b136cc8727d1dfe5ff9419f0ca
munaradev <munarasedieva995@gmail.com>
2024-01-29 15:56:07 +0600

    new page

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+let btn = document.querySelector(".btnThree"),
+  body = document.querySelector("body");
+
+btn.addEventListener("click", () => {
+  if (body.className === "white") {
+    body.classList.replace("white", "dark");
+  } else {
+    body.classList.replace("dark","white");
+  }
+});
+
+
+
+
