3d4f003d2db7263cc9929d2220951d2363a52120
munaradev <munarasedieva995@gmail.com>
2024-02-12 16:27:05 +0600

    адаптивка

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  slidesPerView: 1,
+  spaceBetween: 10,
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+  breakpoints: {
+      "@450": {
+          slidesPerView: 1,
+          spaceBetween: 10,
+      },
+      "992": {
+          slidesPerView: 2,
+          spaceBetween: 20,
+      },
+      "1024": {
+          slidesPerView: 2.5,
+          spaceBetween: 40,
+      },
+      "1440": {
+          slidesPerView: 3,
+          spaceBetween: 50,
+      },
   },
 });
 
