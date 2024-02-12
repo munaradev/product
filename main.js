0e7201b86c4faab08d5373cac823db46625bc6c3
munaradev <munarasedieva995@gmail.com>
2024-02-12 20:24:51 +0600

    adaptivka

--- main.js
+++ main.js
@@ -1,29 +1,4 @@
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-  },
-  breakpoints: {
-      "@450": {
-          slidesPerView: 1,
-          spaceBetween: 10,
-      },
-      "992": {
-          slidesPerView: 2,
-          spaceBetween: 20,
-      },
-      "1024": {
-          slidesPerView: 2.5,
-          spaceBetween: 40,
-      },
-      "1440": {
-          slidesPerView: 3,
-          spaceBetween: 50,
-      },
-  },
-});
+
 
 let btn = document.querySelector(".btnThree"),
   body = document.querySelector("body");
@@ -32,10 +7,49 @@ btn.addEventListener("click", () => {
   if (body.className === "white") {
     body.classList.replace("white", "dark");
   } else {
-    body.classList.replace("dark","white");
+    body.classList.replace("dark", "white");
   }
 });
 
+console.log(btn);
+
+const open = document.querySelector(".menu-open"),
+  close = document.querySelector(".menu-close"),
+  burgerMenu = document.querySelector(".burger-menu");
+
+open.addEventListener("click" ,() =>{
+  burgerMenu.style.display = "block"
+})
 
 
+close.addEventListener("click" ,() =>{
+  burgerMenu.style.display = "none"
+})
 
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@450": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 2.5,
+      spaceBetween: 40,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
